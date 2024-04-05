a637f23f797c89f265862824f9ea571d95df9886
ajiatuo.cn <114716818+ajiatuo@users.noreply.github.com>
2024-04-06 01:45:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到伽陀之语', url: '' },
-    { title: '访问文档中心获取更多帮助', url: '' }
+    { title: '欢迎来到伽陀之语', url: 'http://ajiatuo.cn' },
+    { title: '', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '生命觉醒',
+  HEO_HERO_TITLE_2: '与心灵探索',
+  HEO_HERO_TITLE_3: 'jiatuo.cn',
+  HEO_HERO_TITLE_4: '星际视角',
+  HEO_HERO_TITLE_5: '昴宿二信息传递',
+  HEO_HERO_TITLE_LINK: 'https://ajiatuo.cn',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
